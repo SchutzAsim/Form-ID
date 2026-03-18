0edb07dae0177ef8f219b70ae5460cb10589bdfb
SchutzAsim <asimsaifioffical12@gmail.com>
2026-03-18 06:51:16 +0530

    add docker files

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,16 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host = os.getenv("SEVER_HOST")
+port = int(os.getenv("SERVER_PORT"))
+reload = bool(os.getenv("SERVER_RELOAD"))
 
 if __name__ == "__main__":
-    uvicorn.run(app="app.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app="app.app:app",
+                host=host,
+                port=port,
+                reload=reload
+                )
