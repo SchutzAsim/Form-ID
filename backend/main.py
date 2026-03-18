85e27d80fe8ac93038ea53f42322af03b131fa90
SchutzAsim <asimsaifioffical12@gmail.com>
2026-03-18 09:03:59 +0530

    Docker Caontainer Build Seccessfull

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,9 @@
-import os
 import uvicorn
-from dotenv import load_dotenv
 
-load_dotenv()
-
-host = os.getenv("SEVER_HOST")
-port = int(os.getenv("SERVER_PORT"))
-reload = bool(os.getenv("SERVER_RELOAD"))
 
 if __name__ == "__main__":
     uvicorn.run(app="app.app:app",
-                host=host,
-                port=port,
-                reload=reload
+                host="0.0.0.0",
+                port=8181,
+                reload=True
                 )
